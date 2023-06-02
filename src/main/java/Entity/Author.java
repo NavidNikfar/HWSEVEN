6a686a60bc9f44328b7ff7716e58b6c1f436d4d9
navid git <navid.nikfar@gmail.com>
2023-06-02 13:58:04 +0330

    create Author.java class

--- src/main/java/Entity/Author.java
+++ src/main/java/Entity/Author.java
@@ -1,4 +1,81 @@
 package Entity;
 
+import java.util.Arrays;
+
 public class Author {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String[] books;
+
+    public Author() {
+
+    }
+
+    public Author(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.books = new String[0];
+    }
+
+
+    public Author(long id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String[] getBooks() {
+        return books;
+    }
+
+    public void setBooks(String[] books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", Books=" + Arrays.toString(books) +
+                '}';
+    }
 }
