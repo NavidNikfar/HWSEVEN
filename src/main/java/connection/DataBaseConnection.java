61c55503193722bb18dcfffaaf6eb739ad3857c1
navid git <navid.nikfar@gmail.com>
2023-06-02 13:53:18 +0330

    creat DataBaseConnection.java class

--- src/main/java/connection/DataBaseConnection.java
+++ src/main/java/connection/DataBaseConnection.java
@@ -1,4 +1,27 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DataBaseConnection {
+    static final String URL = "jdbc:postgresql://localhost:5432/HWSeven";
+    static final String USER_NAME = "postgres";
+    static final String PASSWORD = "myjava123";
+
+    private static Connection connection = null;
+
+    private DataBaseConnection() {
+    }
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
 }
