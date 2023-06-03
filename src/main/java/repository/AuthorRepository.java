f600047181dcfde3c44b58ae63ba8e3518bb5319
navid git <navid.nikfar@gmail.com>
2023-06-03 17:03:31 +0330

    add methods for AuthorRepository.java and BookRepository.java classes (without body)

--- src/main/java/repository/AuthorRepository.java
+++ src/main/java/repository/AuthorRepository.java
@@ -1,4 +1,14 @@
 package repository;
 
+import Entity.Author;
+
 public class AuthorRepository {
+    public void save(Author author) {
+
+    }
+
+
+    public Author load(int authorId) {
+
+    }
 }
--- src/main/java/repository/BookRepository.java
+++ src/main/java/repository/BookRepository.java
@@ -1,4 +1,18 @@
 package repository;
 
+import Entity.Book;
+
+
 public class BookRepository {
+    public static void save(Book book) {
+
+    }
+
+    public static Book load(int bookId) {
+
+    }
+
+    public static void delete(int bookId) {
+
+    }
 }
